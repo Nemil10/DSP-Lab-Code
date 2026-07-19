c8389fe58ec54903212ef665567aa584e2e3f7b8
Nemil10 <nemilchhagani@gmail.com>
2026-07-19 16:21:37 +0530

    Create Experiment1_1.py

---
+++ lab 1/Experiment1_1.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def unit_impulse(length, position):
+    signal = np.zeros(length)
+    signal[position] = 1
+    return signal
+
+# Parameters
+start = -10
+stop = 10 
+step = 1 
+x = np.arange(start, stop+step, step)
+
+impulse_signal = unit_impulse(len(x), abs(start)//step)
+
+# Plot the signal
+plt.stem(x, impulse_signal)
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.title('Unit Impulse Signal')
+plt.grid(True)
+plt.show()
